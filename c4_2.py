85eabcd429df6d2c734170cf875c567ee547f6f1
zuowu <zuowu@staff.weibo.com>
2018-06-05 22:05:36 +0800

    123

--- c4_2.py
+++ c4_2.py
@@ -4,6 +4,7 @@ from pygame.locals import *
 from sys import exit
 
 pygame.init()
+# help(pygame)
 screen = pygame.display.set_mode((640, 480) ,0, 32)
 
 font = pygame.font.Font("Arial Unicode.ttf", 40)
