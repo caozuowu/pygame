11aef26a365b47fb245ba0c601b1a437fa6a54b0
zuowu <zuowu@staff.wei.com>
2018-06-06 17:31:29 +0800

    123

--- c7_2.py
+++ c7_2.py
@@ -6,12 +6,15 @@ from math import pi
 pygame.init()
 SCREEN_SIZE = (640, 480)
 screen = pygame.display.set_mode(SCREEN_SIZE, 0, 32)
-center = (SCREEN_SIZE[0]/2, SCREEN_SIZE[1]/2)
-radius = 100
-diameter = 100
+center = [SCREEN_SIZE[0]/2, SCREEN_SIZE[1]/2]
+radius = 5
 angle = 0
 
-
+def drawArc(surface, color, center, radius, a1, a2, ld):
+    rect = (center[0] - radius, center[1] - radius, radius * 2, radius *2)
+    pygame.draw.arc(surface, color, rect, a1, a2, ld)
+    # pygame.draw.arc(screen, rc, (rect),angle, angle + pi/2, 3)
+    pass
 while True:
     
     for event in pygame.event.get():
@@ -19,11 +22,19 @@ while True:
             exit()
         if event.type == pygame.MOUSEBUTTONDOWN:
             rc = (randint(0,255), randint(0,255), randint(0,255))
-            # rp = (randint(0,639), randint(0,479))
-            # rs = (639-randint(rp[0], 639), 479-randint(rp[1], 479))
-            # pygame.draw.rect(screen, rc, Rect(center, (radius,radius)))
-            pygame.draw.arc(screen, rc, (center[0],center[1],radius,radius),angle, angle + pi/2, 3)
-            # radius *= 1.618
+            drawArc(screen, rc, (center), radius, angle, angle + pi/2, 3)
             angle -= pi/2
+            dr = 1.618 * radius - radius
+            radius *= 1.618
+            step = int((-angle*2/pi)%4)
+            print(step)
+            if step == 0:
+                center[1] += dr
+            elif step == 1:
+                center[0] -= dr
+            elif step == 2:
+                center[1] -= dr
+            elif step == 3:
+                center[0] += dr
 
     pygame.display.update()
